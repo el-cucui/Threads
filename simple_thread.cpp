b7292127e0d9a6efad68c133e3d73d31e2dcdacb
Ayub <sayyub404@gmail.com>
2025-04-20 17:33:02 +0530

    First commit

---
+++ simple_thread.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <pthread.h>
+using namespace std;
+
+int a = 0;
+void *thread_function(void *arg)
+{
+    cout << (char *)arg << endl;
+    for (int i = 0; i < 1000000; i++)
+    {
+        a++;
+    }
+    return NULL;
+}
+
+int main()
+{
+    pthread_t pt, pt2;
+    int rt = pthread_create(&pt, NULL, thread_function, (void *)"A");
+    if (rt != 0)
+        cout << "Error in pthread creation\n";
+    rt = pthread_create(&pt2, NULL, thread_function, (void *)"B");
+    if (rt != 0)
+        cout << "Error in pthread creation 2\n";
+    pthread_join(pt, NULL);
+    pthread_join(pt2, NULL);
+    cout << a << endl;
+}
